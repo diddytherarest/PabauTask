c0358ee80e1fecf17725473ec852cf7e7403dc4a
Diar <di68154@ubt-uni.net>
2025-08-27 17:52:46 +0200

    Design
    Added photos to Guitar Brands to make it more attractive

--- src/app/components/BrandCard.tsx
+++ src/app/components/BrandCard.tsx
@@ -14,20 +14,49 @@ export default function BrandCard({ brand }: { brand: Brand }) {
       tabIndex={-1}
     >
       <div className="h-20 flex items-center justify-center">
-        {brand.logoUrl ? (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            src={brand.logoUrl}
-            alt={`${brand.name} logo`}
-            className="max-h-20 max-w-full object-contain"
-            loading="lazy"
-          />
-        ) : (
-          <div
-            className="h-14 w-14 rounded-full bg-black/5 dark:bg-white/10"
-            aria-hidden
-          />
-        )}
+        {(() => {
+          const brandImages: Record<string, string> = {
+            'Fender': '/Fender.jpg',
+            'Ibanez': '/Ibanez.jpg',
+            'Gibson': '/Gibson.jpg',
+            'PRS': '/PRS.jpg',
+            'Martin': '/Martin.jpg',
+            'Yamaha': '/Yamaha.jpg',
+            'Gretsch': '/Gretsch.jpg',
+            'Epiphone': '/Epiphone.jpg',
+            'Jackson': '/Jackson.jpg',
+            'Music Man': '/MusicMan.jpg',
+          };
+          const imgSrc = brandImages[brand.name];
+          if (imgSrc) {
+            return (
+              <img
+                src={imgSrc}
+                alt={`${brand.name} logo`}
+                className="h-16 w-16 object-contain rounded-full border-4 border-yellow-400 shadow-lg bg-white"
+                style={{ background: 'radial-gradient(circle, #fff 60%, #ffd700 100%)' }}
+                loading="lazy"
+              />
+            );
+          } else if (brand.logoUrl) {
+            return (
+              // eslint-disable-next-line @next/next/no-img-element
+              <img
+                src={brand.logoUrl}
+                alt={`${brand.name} logo`}
+                className="max-h-20 max-w-full object-contain"
+                loading="lazy"
+              />
+            );
+          } else {
+            return (
+              <div
+                className="h-14 w-14 rounded-full bg-black/5 dark:bg-white/10"
+                aria-hidden
+              />
+            );
+          }
+        })()}
       </div>
 
       <div
